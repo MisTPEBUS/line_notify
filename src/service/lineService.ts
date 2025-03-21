53cbb5e21d4e62aae4c51a9803d566d923ee47ab
Lobinda <lobinda123@gmail.com>
2025-03-21 13:12:51 +0800

    test

--- src/service/lineService.ts
+++ src/service/lineService.ts
@@ -5,8 +5,38 @@ export const sendMsgService = async (user: createUserType, company: string, msg?
   let token =
     company === 'T' ? (process.env.LINE_CHANNEL_ACCESS_TOKEN_TP ? process.env.LINE_CHANNEL_ACCESS_TOKEN_CP : '') : '';
 
+  const configMapping = {
+    88764: process.env.LINE_CHANNEL_ACCESS_TOKEN_TP,
+  };
   const messageText = msg ?? '這是一則範例訊息';
   console.log(123);
+  const payload = {
+    to: user.userId,
+    messages: [
+      {
+        type: 'text',
+        text: `\n${messageText}\n`,
+      },
+    ],
+  };
+
+  await axios.post('https://api.line.me/v2/bot/message/push', payload, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const sendMsgService = async (user: createUserType, channelId: string, msg?: string) => {
+  let token =
+    company === 'T' ? (process.env.LINE_CHANNEL_ACCESS_TOKEN_TP ? process.env.LINE_CHANNEL_ACCESS_TOKEN_CP : '') : '';
+
+  const configMapping = {
+    88764: process.env.LINE_CHANNEL_ACCESS_TOKEN_TP,
+  };
+  const messageText = msg ?? '這是一則範例訊息';
+
   const payload = {
     to: user.userId,
     messages: [
