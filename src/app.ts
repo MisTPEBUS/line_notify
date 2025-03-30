a1637f01e305802768fb0575b2f6996b51f35d41
Lobinda <lobinda123@gmail.com>
2025-03-30 17:56:51 +0800

    options

--- src/app.ts
+++ src/app.ts
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 // Root Route
 app.use('/v1/api/lineHook', Router);
-app.get('/OPTION', (req: Request, res: Response) => {
+app.options('/', (req: Request, res: Response) => {
   res.status(200).json();
 });
 
